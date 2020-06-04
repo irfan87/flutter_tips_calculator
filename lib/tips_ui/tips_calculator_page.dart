ebf3efab7bf71f0a11a91f093a8937f3abd7429e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-06-04 16:13:40 +0800

    organized files

---
+++ lib/tips_ui/tips_calculator_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TipsCalculatorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Tips Calculator'),
+          ],
+        ),
+      ),
+    );
+  }
+}
