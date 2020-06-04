9ff69b3abb8b08028fd520a42dc7ee50ed93d03f
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-06-04 16:29:28 +0800

    applied MediaQuery for height so other devices can view properly

--- lib/tips_ui/tips_calculator_page.dart
+++ lib/tips_ui/tips_calculator_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 
-class TipsCalculatorPage extends StatelessWidget {
+class TipsCalculatorPage extends StatefulWidget {
+  @override
+  _TipsCalculatorPageState createState() => _TipsCalculatorPageState();
+}
+
+class _TipsCalculatorPageState extends State<TipsCalculatorPage> {
+  int _tipsPercentage = 0;
+  int _personCounter = 1;
+  double _billAmmount = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(20.5),
           children: <Widget>[
-            Text('Tips Calculator'),
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.purpleAccent.shade100,
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Text("Total Per Person"), Text("\$123")],
+              ),
+            ),
           ],
         ),
       ),
